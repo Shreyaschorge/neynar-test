5525e30d0fc07475f7625afb66591ea295596df8
Shreyaschorge <shreyas@neynar.com>
2023-10-12 20:13:48 +0530

    Patch update: v0.0.6

--- version-update.ts
+++ version-update.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import readline from 'readline';
+import { execSync } from 'child_process';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,18 +21,22 @@ function main(): void {
     // Prompt user for the type of change
     rl.question('Is this update a major(mj), minor(mi), or patch(p)? ', (type) => {
       let newVersion = '';
+      let commitMessage = '';
       switch (type.toLowerCase()) {
         case 'major':
         case 'mj':
           newVersion = `${parseInt(currentVersion[0]) + 1}.0.0`;
+          commitMessage = 'Major update';
           break;
         case 'minor':
         case 'mi':
           newVersion = `${currentVersion[0]}.${parseInt(currentVersion[1]) + 1}.0`;
+          commitMessage = 'Minor update';
           break;
         case 'patch':
         case 'p':
           newVersion = `${currentVersion[0]}.${currentVersion[1]}.${parseInt(currentVersion[2]) + 1}`;
+          commitMessage = 'Patch update';
           break;
         default:
           console.log('Invalid input. Input must be: major, minor, or patch.');
@@ -46,6 +51,15 @@ function main(): void {
           process.exit(1);
         }
         console.log(`Updated package.json version to ${newVersion}`);
+
+        // Git add, commit and push
+        try {
+            execSync('git add -A', { stdio: 'inherit' });
+            execSync(`git commit -m "${commitMessage}: v${newVersion}"`, { stdio: 'inherit' });
+            console.log('Committed with message: ' + `"${commitMessage}: v${newVersion}"`);
+          } catch (error) {
+            console.error('Error occurred while committing:', error);
+          }
         rl.close();
       });
     });
