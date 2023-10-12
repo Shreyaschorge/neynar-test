4930a7150d49ed3d6b53c03ae852a467ac0446cb
Shreyaschorge <shreyas@neynar.com>
2023-10-12 17:33:36 +0530

    Add greet class

---
+++ src/greetings/greet/greet.ts
@@ -0,0 +1,20 @@
+
+
+export class Greetings {
+    constructor(public name: string, public age: number) {
+        if(this.age < 12) {
+            throw new Error("You are not allowed to enter");
+        } else {
+            console.log("------------- Welcome --------------");
+        }
+    }
+
+    sayHello() {
+        console.log(`Hello 🙋‍♀️ ${this.name}...!!!\nIts good to see you here.`)
+    }
+
+    sayHi() {
+        console.log(`Hi 🙋 ${this.name}...!!!\nIts good to see you here. `)
+    }
+
+}
